07b6896b7a0ac7d636d9012ca3c69dea11b89cc5
Dario Carlomagno <dario.carlomagno@gmail.com>
2016-02-08 12:07:14 +0100

    proj creation

---
+++ DesignPatternTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DesignPatternTest
+//
+//  Created by Dario Carlomagno on 08/02/16.
+//  Copyright © 2016 Dario Carlomagno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DesignPatternTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesignPatternTest
+//
+//  Created by Dario Carlomagno on 08/02/16.
+//  Copyright © 2016 Dario Carlomagno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
