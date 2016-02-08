c33fde81a0060a95c7cd201c768f1bfc6234c279
Dario Carlomagno <dario.carlomagno@gmail.com>
2016-02-08 13:48:13 +0100

    added horizontalscroller

--- DesignPatternTest/Controller/ViewController.m
+++ DesignPatternTest/Controller/ViewController.m
@@ -7,21 +7,83 @@
 //
 
 #import "ViewController.h"
+#import "LibraryAPI.h"
+#import "Album+TableRepresentation.h"
 
-@interface ViewController ()
+@interface ViewController ()  <UITableViewDataSource, UITableViewDelegate> {
+    UITableView *dataTableView;
+    NSArray *allAlbums;
+    NSDictionary *currentAlbumData;
+    int currentAlbumIndex;
+}
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor colorWithRed:0.76f green:0.81f blue:0.87f alpha:1];
+    currentAlbumIndex = 0;
+    
+    allAlbums = [[LibraryAPI sharedInstance] getAlbums];
+    [self setupTableView];
+    
+    [self showDataForAlbumAtIndex:currentAlbumIndex];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)setupTableView {
+    dataTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 120, self.view.frame.size.width, self.view.frame.size.height-120) style:UITableViewStyleGrouped];
+    dataTableView.delegate = self;
+    dataTableView.dataSource = self;
+    dataTableView.backgroundView = nil;
+    [self.view addSubview:dataTableView];
+}
+
+- (void)showDataForAlbumAtIndex:(int)albumIndex
+{
+    // defensive code: make sure the requested index is lower than the amount of albums
+    if (albumIndex < allAlbums.count)
+    {
+        // fetch the album
+        Album *album = allAlbums[albumIndex];
+        // save the albums data to present it later in the tableview
+        currentAlbumData = [album tr_tableRepresentation];
+    }
+    else
+    {
+        currentAlbumData = nil;
+    }
+    
+    // we have the data we need, let's refresh our tableview
+    [dataTableView reloadData];
+}
+
+#pragma mark - TableView delegate
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [currentAlbumData[@"titles"] count];
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (!cell)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
+    }
+    
+    cell.textLabel.text = currentAlbumData[@"titles"][indexPath.row];
+    cell.detailTextLabel.text = currentAlbumData[@"values"][indexPath.row];
+    
+    return cell;
 }
 
 @end
